01ce720a725fd621fb77ab6dc0b4c927c4d753b8
phuongnam <zdpnam1@.com>
2025-04-21 16:47:59 +0700

    2 noti

--- app/src/main/java/com/example/loginform/MainActivity.kt
+++ app/src/main/java/com/example/loginform/MainActivity.kt
@@ -16,7 +16,8 @@ import java.util.Date
 
 class MainActivity : AppCompatActivity() {
 
-    private val channelId = "my_channel_id"
+    private val channelId1 = "Chanel1"
+    private val channelId2 = "Chanel2"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,17 +25,41 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val btnSendNotification = findViewById<Button>(R.id.btn_send_notification)
-        btnSendNotification.setOnClickListener {
-            sendNotification()
+        // Tạo notification channels nếu cần
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+//
+//            val channel1 = android.app.NotificationChannel(
+//                channelId1,
+//                "Channel 1",
+//                NotificationManager.IMPORTANCE_DEFAULT
+//            )
+//            notificationManager.createNotificationChannel(channel1)
+//
+//            val channel2 = android.app.NotificationChannel(
+//                channelId2,
+//                "Channel 2",
+//                NotificationManager.IMPORTANCE_DEFAULT
+//            )
+//            notificationManager.createNotificationChannel(channel2)
+//        }
+
+        val btnSendNotification1 = findViewById<Button>(R.id.btn_send_notification1)
+        btnSendNotification1.setOnClickListener {
+            sendNotification1()
         }
 
+        val btnSendNotification2 = findViewById<Button>(R.id.btn_send_notification2)
+        btnSendNotification2.setOnClickListener {
+            sendNotification2()
+        }
     }
-    private fun sendNotification() {
+
+    private fun sendNotification1() {
         val bitmap = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher)
 
         val builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            Notification.Builder(this, channelId)
+            Notification.Builder(this, channelId1)
         } else {
             Notification.Builder(this)
         }
@@ -42,6 +67,28 @@ class MainActivity : AppCompatActivity() {
         val notification = builder
             .setContentTitle("Hello!")
             .setContentText("This is a simple push notification.")
+            .setSmallIcon(R.drawable.img) // bạn có thể dùng ic_launcher nếu chưa có
+            .setLargeIcon(bitmap)
+            .setColor(ContextCompat.getColor(this, R.color.teal_200)) // hoặc bất kỳ màu nào bạn muốn
+            .build()
+
+        val notificationManager =
+            getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(getNotificationId(), notification)
+    }
+
+    private fun sendNotification2() {
+        val bitmap = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher)
+
+        val builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Notification.Builder(this, channelId2)
+        } else {
+            Notification.Builder(this)
+        }
+
+        val notification = builder
+            .setContentTitle("Bye!")
+            .setContentText("This is a chanel 2 notification.")
             .setSmallIcon(R.drawable.img) // bạn có thể dùng ic_launcher nếu chưa có
             .setLargeIcon(bitmap)
             .setColor(ContextCompat.getColor(this, R.color.teal_200)) // hoặc bất kỳ màu nào bạn muốn
