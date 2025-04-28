82a8137ac884f548fe6621430397cafa4a63203b
phuongnam <zdpnam1@.com>
2025-04-28 11:09:29 +0700

    notification

--- app/src/main/java/com/example/loginform/MainActivity.kt
+++ app/src/main/java/com/example/loginform/MainActivity.kt
@@ -13,89 +13,87 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.util.Date
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
     private val channelId1 = "Chanel1"
     private val channelId2 = "Chanel2"
+    private val title_push="What is Lorem Ipsum?\n"
+    private val content_push=" when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, " +
+            "but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised " +
+            "in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishin"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        // Tạo notification channels nếu cần
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 //
-//            val channel1 = android.app.NotificationChannel(
-//                channelId1,
-//                "Channel 1",
-//                NotificationManager.IMPORTANCE_DEFAULT
-//            )
-//            notificationManager.createNotificationChannel(channel1)
+//        val btnSendNotification1 = findViewById<Button>(R.id.btn_send_notification1)
+//        btnSendNotification1.setOnClickListener {
+//            sendNotification1()
+//        }
 //
-//            val channel2 = android.app.NotificationChannel(
-//                channelId2,
-//                "Channel 2",
-//                NotificationManager.IMPORTANCE_DEFAULT
-//            )
-//            notificationManager.createNotificationChannel(channel2)
+//        val btnSendNotification2 = findViewById<Button>(R.id.btn_send_notification2)
+//        btnSendNotification2.setOnClickListener {
+//            sendNotification2()
 //        }
 
-        val btnSendNotification1 = findViewById<Button>(R.id.btn_send_notification1)
-        btnSendNotification1.setOnClickListener {
-            sendNotification1()
-        }
+        // MyFirebaseMessage
+        FirebaseMessaging.getInstance().subscribeToTopic("News")
+            .addOnSuccessListener {
+                println("Subscribe success")
+            }
+            .addOnFailureListener {
+                println("Subscribe fail")
+            }
 
-        val btnSendNotification2 = findViewById<Button>(R.id.btn_send_notification2)
-        btnSendNotification2.setOnClickListener {
-            sendNotification2()
-        }
     }
 
     private fun sendNotification1() {
         val bitmap = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher)
 
-        val builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            Notification.Builder(this, channelId1)
-        } else {
-            Notification.Builder(this)
-        }
+        val builder = NotificationCompat.Builder(this, channelId1) // dùng NotificationCompat.Builder
+
+        //Set âm thanh
+        builder.setDefaults(Notification.DEFAULT_SOUND)
 
         val notification = builder
-            .setContentTitle("Hello!")
-            .setContentText("This is a simple push notification.")
-            .setSmallIcon(R.drawable.img) // bạn có thể dùng ic_launcher nếu chưa có
+            .setContentTitle(title_push)
+            .setContentText(content_push)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText(content_push)
+            )
+            .setSmallIcon(R.drawable.img)
             .setLargeIcon(bitmap)
-            .setColor(ContextCompat.getColor(this, R.color.teal_200)) // hoặc bất kỳ màu nào bạn muốn
+            .setColor(ContextCompat.getColor(this, R.color.teal_200))
             .build()
 
-        val notificationManager =
-            getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(getNotificationId(), notification)
     }
 
     private fun sendNotification2() {
-        val bitmap = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher)
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.sky)
 
-        val builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            Notification.Builder(this, channelId2)
-        } else {
-            Notification.Builder(this)
-        }
+        val builder = NotificationCompat.Builder(this, channelId2) // cũng dùng NotificationCompat.Builder
 
         val notification = builder
             .setContentTitle("Bye!")
             .setContentText("This is a chanel 2 notification.")
-            .setSmallIcon(R.drawable.img) // bạn có thể dùng ic_launcher nếu chưa có
+            .setSmallIcon(R.drawable.img)
+            .setStyle(
+                NotificationCompat.BigPictureStyle()
+                    .bigPicture(bitmap)
+            )
             .setLargeIcon(bitmap)
-            .setColor(ContextCompat.getColor(this, R.color.teal_200)) // hoặc bất kỳ màu nào bạn muốn
+            .setColor(ContextCompat.getColor(this, R.color.teal_200))
             .build()
 
-        val notificationManager =
-            getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(getNotificationId(), notification)
     }
 
